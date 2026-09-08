6f45df6fe15a2afc7b9c4848db0ac01db0a96413
AzharAndriann <azhardwiandrian@gmail.com>
2026-09-08 17:01:31 +0700

    update something

--- src/main.js
+++ src/main.js
@@ -13,7 +13,7 @@ function Main ({activeNote, onUpdateNote}) {
 
     if(!activeNote) return <div className="no-active-note">no note selected </div>
 
-    return <div className="app=main">
+    return <div className="app-main">
         <div className="app-main-note-edit">
         <input type="text" id="title" value={activeNote.title} onChange={(e) => onEditField("title",e.target.value )} autoFocus />
         <textarea id="body"  placeholder="Write your note here..."  value={activeNote.body} onChange={(e) => onEditField("body",e.target.value )}/>
